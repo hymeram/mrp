1ab5de3d1eebe02246ce8a20cbd3c3d41b20a46a
hymeram <hymeram@gmail.com>
2022-11-08 22:43:04 +0000

    add example

---
+++ MRP Example - national identity in the UK/national_identity_mrp.R
@@ -0,0 +1,16 @@
+# libraries
+
+# import data
+
+# clean data to match poststratification frame (psf)
+
+# build model
+
+# model diagnostics
+
+# predict psf categories
+
+# compare estimates to direct estimates
+
+# map estimates
+
