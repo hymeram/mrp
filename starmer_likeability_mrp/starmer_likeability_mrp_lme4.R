31d8360c34edc597f398d9b75808eb967c51d5fb
hymeram <hymeram@gmail.com>
2022-11-13 00:14:17 +0000

    starmer likeability example

--- starmer_likeability_mrp/starmer_likeability_mrp_lme4.R
+++ starmer_likeability_mrp/starmer_likeability_mrp_lme4.R
@@ -2,6 +2,8 @@
 library(haven)
 library(tidyverse)
 library(lme4)
+library(sf)
+library(scico)
 options(scipen = 999)
 
 setwd("MRP example - UK national identity")
@@ -17,19 +19,21 @@ bes <- haven::read_dta(bes_location)
 # import constituency level predictors from BES
 aux_location <- "C:/Users/Alex/Documents/Data/BES-2019-General-Election-results-file-v1.1.xlsx"
 aux <- readxl::read_excel(aux_location) %>%
-  select(pano,
-         ONSConstID,
-         Country,
-         Con19,
-         Lab19,
-         SNP19,
-         PC19,
-         Brexit19,
-         leaveHanretty,
-         c11PopulationDensity,
-         c11BornUK,
-         c11Retired,
-         c11DeprivedNone) %>%
+  select(
+    ConstituencyName,
+    pano,
+    ONSConstID,
+    Country,
+    Region,
+    Winner19,
+    Con19,
+    Lab15,
+    Lab19,
+    LD19,
+    leaveHanretty,
+    c11EthnicityWhiteBritish,
+    c11Retired,
+    c11DeprivedNone) %>%
   replace(is.na(.), 0)
 
 # import poststratification frame ----------------------------------------------
@@ -43,15 +47,11 @@ psf <- read.csv(psf_location, stringsAsFactors = FALSE)
 # clean data to match psf ------------------------------------------------------
 
 bes_clean <- bes %>%
-  select(
-    britishness,englishness,scottishness,welshness,
-    country,gor,pano,pcon,gender,age,p_housing,p_education,p_socgrade) %>%
+  select(likeStarmer, likeLab, gor,pano,pcon,gender,age,p_housing,p_education,p_socgrade) %>%
+  mutate(likeStarmer = as.integer(likeStarmer),
+         likeLab = as.integer(likeLab)) %>%
   na_if(9999) %>%
-  mutate_at(vars("britishness","englishness","scottishness","welshness"), replace_na, 1) %>%
-  mutate_at(vars("britishness","englishness","scottishness","welshness"), as.numeric) %>%
-  as_factor() %>%
-  mutate(britishness = britishness-1)
-
+  as_factor()
 
 bes_clean <- bes_clean %>%
   mutate(
@@ -86,7 +86,7 @@ bes_clean <- bes_clean %>%
     age0 = cut(as.numeric(age), 
       breaks=c(-Inf, 19, 24, 29, 44, 59, 64, 74, Inf), 
       labels=c("16-19","20-24","25-29","30-44","45-59","60-64","65-74","75+"))) %>%
-  select(-age,-p_housing,-p_education,-p_socgrade,-englishness,-scottishness,-welshness) 
+  select(-age,-p_housing,-p_education,-p_socgrade) 
 
 # merge with aux data 
 df <- bes_clean %>%
@@ -94,64 +94,112 @@ df <- bes_clean %>%
 
 # build model ------------------------------------------------------------------
 
-model <-  lme4::lmer(
-  'britishness ~ 
-  (1|age0) + 
-  (1|education) + 
+# Like Starmer
+starmer_model <-  lme4::lmer(
+  'likeStarmer ~ 
   (1|pcon) + 
-  (1|age0:country) + 
-  (1|education:country) + 
+  (1|gor) +
+  (1|age0) + 
+  (1|age0:sex) + 
+  (1|education) + 
+  (1|hrsocgrd) + 
   sex +
   housing +
-  country +
-  hrsocgrd +
-  Con19 +
-  SNP19 + 
-  Brexit19 + 
-  PC19 +
+  Lab15 + 
+  LD19 +
   leaveHanretty +
-  c11BornUK +
-  c11DeprivedNone',
+  c11DeprivedNone +
+  c11Retired +
+  c11EthnicityWhiteBritish',
   data = df
   )
 
 summary(model)
 
-# model diagnostics -----------------------------------------------------------
-plot(model)
-ranef(model)
-sjPlot::plot_model(model, "re")
-sjPlot::plot_model(model, "est")
+# Like Labour
+labour_model <-  lme4::lmer(
+  'likeLab ~ 
+  (1|pcon) + 
+  (1|gor) +
+  (1|age0) + 
+  (1|education) + 
+  (1|age0:education) + 
+  (1|hrsocgrd) + 
+  sex +
+  housing +
+  Lab15 + 
+  Lab19 + 
+  leaveHanretty +
+  c11DeprivedNone +
+  c11Retired +
+  c11EthnicityWhiteBritish',
+  data = df
+)
+
+# model outputs
+summary(labour_model)
+plot(labour_model)
+ranef(labour_model)
+sjPlot::plot_model(labour_model, "re")
+sjPlot::plot_model(labour_model, "est")
 
 # poststratification -----------------------------------------------------------
 
 # match psf to model
 psf <- psf %>%
   merge(aux, by.x="GSSCode", by.y="ONSConstID") %>%
-  rename(c(country = Country, pcon = GSSCode))
+  rename(c(country = Country, pcon = GSSCode, gor = Region))
 
 # predict onto psf
-psf$est <- predict(model, psf, allow.new.levels=T)
+psf$likeStarmer <- predict(starmer_model, psf, allow.new.levels=T)
+psf$likeLabour <- predict(labour_model, psf, allow.new.levels=T)
 
 # combines estimates by constituency
 mrp_estimates <- psf %>%
-  group_by(country, pcon) %>%
-  summarise(mrp_est = sum(est*weight, na.rm=T)) %>%
-  ungroup()
+  group_by(country, Winner19, ConstituencyName, pcon) %>%
+  summarise(likeStarmer = sum(likeStarmer*weight, na.rm=T),
+            likeLabour = sum(likeLabour*weight, na.rm=T))
 
-# compare estimates to direct estimates
-direct <- df %>%
-  group_by(country, ONSConstID) %>%
-  summarise(direct_est = mean(britishness, na.rm=T)) 
+# plot comparison
+ggplot(mrp_estimates, aes(y=likeLabour, x=likeStarmer)) +
+  geom_point(aes(colour = Winner19), size=2) +
+  theme_bw() + 
+  scale_colour_manual(values = c(
+    "blue", "darkgreen", "red", "orange", "green", "gold", "grey"
+    ))
 
-direct %>%
-  merge(.,mrp_estimates, by.x="ONSConstID", by.y="pcon") %>%
-  slice_sample(n = 100) %>%
-  ggplot(aes(y=forcats::fct_reorder(ONSConstID, mrp_est+direct_est/2))) +
-  geom_point(aes(x=direct_est), colour="red") +
-  geom_point(aes(x=mrp_est), colour="blue")
-
-# display data ---------------------------------------------------------------
+# display data -----------------------------------------------------------------
 
 # map estimates
+shapefile <- sf::read_sf("C:/Users/Alex/Documents/Data/Constituencies/Westminster_Parliamentary_Constituencies_(December_2019)_Boundaries_UK_BUC.shp") %>%
+  merge(mrp_estimates, by.y = "pcon", by.x = "pcon19cd") %>%
+  sf::st_transform(., 27700) %>%
+  pivot_longer(., cols = c(likeLabour, likeStarmer))
 
+labs <- c("Like Labour", "Like Starmer")
+names(labs) <- c("likeLabour", "likeStarmer")
+
+# contrast likeability
+map <- ggplot(shapefile) +
+  geom_sf(aes(fill=value), colour = "white", size = NA) + 
+  theme_bw() +
+  facet_wrap(~name, labeller = labeller(name = labs)) +
+  labs(title = "Comparison of the Likeability of Labour and Kier Starmer",
+       subtitle = "Constituency estimates modelled using Multilevel Regression and Poststratification",
+       caption = "Please note the midpoint of the colour scale is not set to 5") +
+  scale_fill_gradient2(
+    low = "royalblue1", 
+    mid = "gray85",
+    high = "red3",
+    midpoint = mean(shapefile$value),
+    name = "Likeability of Labour\n& Starmer (0-10)") +
+  theme(strip.text = element_text(size = 12, face = "bold"),
+        plot.title = element_text(size = 14, face = "bold"),
+        axis.ticks = element_blank(),
+        axis.text = element_blank(),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank())
+
+map
+
+# where is Kier an asset
